82828d1de6560001c786f8b56becbd56b06e5892
NastyaPisankinaaaaaqq <pisankinaa@yandex.ru>
2019-04-16 14:12:17 +0700

    Initial commit

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Room:
+    #TODO
+    #две таблички здесь!
+
+
+class Client:
+    #TODO
+
+
+class Accomodations_options(Room):
+    #TODO
---
+++ main.py
@@ -0,0 +1,13 @@
+def read_room():
+    #TODO
+
+def forming():
+    lst = {''}
+
+
+def main():
+    #TODO
+
+
+if __name__ == "__main__":
+    main()
